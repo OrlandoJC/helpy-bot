c825acfe4f8b19c8f69b8015e728418e078172a4
orlandoJc <cuximyamaorlandodejesus@gmail.com>
2022-07-31 13:51:46 -0500

    Fix : add json commands a write on ready message

--- events/ready.js
+++ events/ready.js
@@ -2,9 +2,6 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-        console.log('Ready!');
-        client.fetchInvite('https://discord.gg/jNre42GQ')
-            .then(invite => console.log(`Obtained invite with code: ${invite.code}`))
-            .catch(console.error);
+        console.log('Bot has been initizaled!');
 	},
 };
